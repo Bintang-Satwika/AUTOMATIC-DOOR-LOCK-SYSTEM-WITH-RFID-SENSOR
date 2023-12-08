dbeba3ccb0da2cd0a1daf1bd1e670a5b900b0a95
Satwika Bintang Bahana <87467666+Bintang-Satwika@users.noreply.github.com>
2023-12-08 15:57:09 +0700

    Update gabungan1.ino

--- gabungan1.ino
+++ gabungan1.ino
@@ -1,4 +1,4 @@
-#include <MFRC522.h>
+#include <MFRC522.h> // for RFID
 #include <LiquidCrystal_I2C.h>
 #include <Servo.h>
 #include <Wire.h>   // Include library Wire
@@ -6,69 +6,99 @@
 #include <SPI.h>    // Include library SPI
 #include <SD.h>     // Include library SD
 
-DS3231  rtc(SDA, SCL);
-File    myFile;
-
 #define RST_PIN 5 // arduino mega
 #define SS_PIN  53 // arduino mega
+#define CS_PIN 4 
+#define SERVO_PIN 6
+#define YellowLED  40
+#define GreenLED 41
+#define RedLED 42
+#define Buzzer 7
 
+DS3231  rtc(SDA, SCL); // rtc module for time
+File    myFile; // SD CARD
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);
+MFRC522 rfid(SS_PIN, RST_PIN);// RFID Sensor
 String MasterTag = "73 6C C8 12"; // Enter you tag UID which we get it from first code. 
-
 String UIDCard = "";
 
+LiquidCrystal_I2C lcd(0x27, 16, 2); // LCD
+Servo servo; // SERVO
+
 int lock=1;
-int live_logging=0;
-bool state=false;
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+String state="false";
 
-Servo servo;
-
-#define YellowLED  2
-#define GreenLED 3
-#define RedLED 6
-
-#define Buzzer 7
 void setup() {
   // put your setup code here, to run once:
+  
   Serial.begin(9600);
+  while(!Serial);
 
   SPI.begin();
-  mfrc522.PCD_Init();
+  rfid.PCD_Init();
 
-  lcd.init();
-  lcd.backlight();
-  lcd.clear();
-
-  servo.attach(A0);
+  servo.attach(SERVO_PIN);
   servo.write(0);
 
   pinMode(GreenLED, OUTPUT);
   pinMode(YellowLED, OUTPUT);
   pinMode(RedLED, OUTPUT);
-
   pinMode(Buzzer, OUTPUT);
-
   digitalWrite(YellowLED, HIGH);
-
+    
+  lcd.init();
+  lcd.backlight();
   lcd.clear();
   lcd.print(" Access Control ");
   lcd.setCursor(0, 1);
   lcd.print("Scan Your Card");
-  setup_logging();
+  
+    // Menuliskan ke serial monitor
+  Serial.println(F(" "));
+  delay(500);
+  
+  // RTC MODULE INISIASI
+  rtc.begin();
+  rtc.setDOW(FRIDAY);     // Set Day-of-Week to SUNDAY
+  rtc.setTime(15, 56, 0);     
+  rtc.setDate(08, 12, 2023);   
+  
+  //  SD CARD INISIASI
+  Serial.print(F("Initializing SD card..."));
+  if(!SD.begin(CS_PIN)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  else {
+    Serial.println(F("Initialization done."));
+    
+    if( SD.exists("LOG.txt") == 0 ) {
+      Serial.print(F("\r\nCreating 'LOG.txt' file..."));
+      myFile = SD.open("LOG.txt", FILE_WRITE);
+      if(myFile) {
+        Serial.println(F("'LOG.txt' file created."));        
+        myFile.println("Date & Time, ID CARD, condition");
+        myFile.close();
+      }
+      else
+        Serial.println(F("Error creating file or file already exist."));
+    }
+      
+  }
 }
 
 void loop() {
+ // Serial.println("Mulai:");
   // put your main code here, to run repeatedly:
   digitalWrite(YellowLED, HIGH);
   digitalWrite(RedLED, LOW);
   digitalWrite(GreenLED, LOW);
   noTone(Buzzer);
   //Wait until new tag is available
-  while (getUID()){
+  while(getUID()){
     Serial.print("UID: ");
     Serial.println(UIDCard);
+    loop_logging(); // DATA LOGGING
     lcd.clear();
     lcd.setCursor(2, 0);
     lcd.print("Permission");
@@ -78,11 +108,10 @@ void loop() {
       digitalWrite(GreenLED, HIGH);
       digitalWrite(YellowLED, LOW);
       digitalWrite(RedLED, LOW);
-      servo.write(100);
+      servo.write(90);
       delay(50);
       lock=0;
-      state = true;
-      live_logging=1;
+      state = "true";
       for (int i = 0; i < 2; i++) {
         tone(Buzzer, 2000);
         delay(250);
@@ -98,8 +127,7 @@ void loop() {
       servo.write(0);
       delay(50);
       lock=1;
-      state = true;
-      live_logging=1;
+      state = "true";
       for (int i = 0; i < 2; i++) {
         tone(Buzzer, 2000);
         delay(250);
@@ -107,13 +135,11 @@ void loop() {
         delay(250);
         }  
       }
-      
     else{
       lcd.print(" Access Denied!");
       digitalWrite(YellowLED, LOW);
       digitalWrite(GreenLED, LOW);
-      state=false;
-      live_logging=1;
+      state="false";
       tone(Buzzer, 2000);
       for(int i = 0; i < 3;i++){
         digitalWrite(RedLED, HIGH);
@@ -123,98 +149,56 @@ void loop() {
         }
       noTone(Buzzer);
     }
-    if(live_logging=1){
-      loop_logging();
-      live_logging=0;
-        }
     delay(1000);
     lcd.clear();
     lcd.print(" Access Control ");
     lcd.setCursor(0, 1);
     lcd.print("Scan Your Card");
-  }
+  } // IF GET UID EALSE
   
-}
-void setup_logging(){
-  
-    while (!Serial) {
-    ;
-  }
-  
-  // Menuliskan ke serial monitor
-  Serial.println(F(" "));
-  delay(500);
-  
-  rtc.begin();
-  
-  // Setting tanggal dan waktu pertama program
-  rtc.setDate(06, 12, 2023);   // Sesuaikan tanggal dengan tanggal praktikum Anda (dd, mm, yyyy)
-  rtc.setTime(11, 02, 00);     // Sesuaikan jam dengan waktu Anda (hh, mm, ss)
-  rtc.setDOW(WEDNESDAY);     // Sesuaikan hari dengan hari Anda praktikum (SUNDAY, MONDAY, etc.)
-
-  Serial.print(F("Initializing SD card..."));
- 
-  if (!SD.begin(4)) {
-    Serial.println(F("Initialization failed!"));
-    while (1);
-  }
-  else {
-      Serial.println(F("Initialization done."));
-      
-      if( SD.exists("LOG.txt") == 0 ) {
-        Serial.print(F("\r\nCreating 'LOG.txt' file..."));
-        myFile = SD.open("LOG.txt", FILE_WRITE);
-        if(myFile) {
-          Serial.println(F("'LOG.txt' file created."));        
-          myFile.println("Date & Time, ID CARD");
-          myFile.close();
-        }
-        else
-          Serial.println(F("Error creating file or file already exist.")); 
-       }
-        
-    }
-  }
+} // VOID LOOP END
 
 void loop_logging(){
-  char buffer1[50], tempStr[6];
+  char buffer1[12], tempStr[6], buffer2[20];
   myFile = SD.open("LOG.txt", FILE_WRITE);
+  //Serial.println("mASUK LOOP");
   if (myFile) {
-    sprintf(buffer1, "Date: %s, Time: %s, Day of the Week: %s, UIDCARD: %s, boolean: %s,", rtc.getDateStr(), rtc.getTimeStr(), rtc.getDOWStr(), UIDCard, state);
+   // Serial.println("MAU PRINT LOGGING 222222");
+    sprintf(buffer1, "Date: %s, Time: %s, Day of the Week: %s\n", rtc.getDateStr(), rtc.getTimeStr(), rtc.getDOWStr());
     Serial.print(buffer1);
+    // Serial.print("ID CARD:" +String(UIDCard));
     // SAVE TO SD CARD
     myFile.print(rtc.getDateStr());
     myFile.print(" ");
     myFile.print(rtc.getTimeStr());
     myFile.print(",");
-    myFile.print(UIDCard);
+    myFile.print(String(UIDCard));
     myFile.print(",");
-    myFile.print(state);
+    myFile.print(String(state));
+    myFile.print("\n");
     myFile.close();
   }
-  else {
-    Serial.println(F("Error opening LOG.txt")); 
-  }
+  else Serial.println(F("Error opening LOG.txt")); 
   
   }
 
-boolean getUID(){
-  if (! mfrc522.PICC_IsNewCardPresent()) {
+bool getUID(){
+  if (! rfid.PICC_IsNewCardPresent()) {
     return false;
   }
 
-  if (! mfrc522.PICC_ReadCardSerial()) {
+  if (! rfid.PICC_ReadCardSerial()) {
     Serial.println("Not able to read the card");
     return false;
   }
   UIDCard = "";
-  for (byte i = 0; i < mfrc522.uid.size; i++) {
-    UIDCard.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-    UIDCard.concat(String(mfrc522.uid.uidByte[i], HEX));
+  for (byte i = 0; i < rfid.uid.size; i++) {
+    UIDCard.concat(String(rfid.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    UIDCard.concat(String(rfid.uid.uidByte[i], HEX));
   }
   UIDCard.toUpperCase();
   UIDCard = UIDCard.substring(1);
 
-  mfrc522.PICC_HaltA();
+  rfid.PICC_HaltA();
   return true;
 }
